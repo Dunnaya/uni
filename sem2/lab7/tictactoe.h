fcc1b5a33686087647cbd5ed9a63e75f8b812f33
Dana <159054921+Dunnaya@users.noreply.github.com>
2024-05-10 21:23:52 +0300

    Update tictactoe.h

--- sem2/lab7/tictactoe.h
+++ sem2/lab7/tictactoe.h
@@ -3,18 +3,25 @@
 
 char board[3][3];
 const char player = 'X';
+const char player2 = 'O';
 const char computer = 'O';
 
 void resetBoard();
 void printBoard();
 int checkFreeSpaces();
 void playerMove();
+void player2Move();
 void computerMove();
 char checkWinner();
 void printWinner(char);
+void printWinner2(char); 
 
 int minimax(bool maximizing);
 
+void startGame1();
+void startGame2();
+void playAgain();
+
 void menu();
 
-#endif // MINIMAX_H
+#endif
