041f9a5d39dbd5c2a57efa2360cf7fbc9b07d30c
Dana <159054921+Dunnaya@users.noreply.github.com>
2024-05-13 18:32:55 +0300

    Update tictactoe.cpp
    Fixed checkWinner() function. Almost finished :^)

--- sem2/lab7/tictactoe.cpp
+++ sem2/lab7/tictactoe.cpp
@@ -181,29 +181,28 @@ void computerMove()
 
 char checkWinner() 
 {
-    //rows
-    for(int i = 0; i <3; i++)
+    for(int i = 0; i < 3; i++)
     {
-        if(board[i][0] == board[i][1] && board[i][0] == board[i][2])
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != ' ')
         {
             return board[i][0];
         }
     }
-    //columns
-    for(int i = 0; i <3; i++)
+
+    for(int i = 0; i < 3; i++)
     {
-        if(board[0][i] == board[1][i] && board[0][i] == board[2][i])
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][i] != ' ')
         {
             return board[0][i];
         }
     }
-    //diagonals
-    if(board[0][0] == board[1][1] && board[0][0] == board[2][2])
+
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != ' ')
     {
         return board[0][0];
     }
 
-    if(board[0][2] == board[1][1] && board[0][2] == board[2][0])
+    if(board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != ' ')
     {
         return board[0][2];
     }
