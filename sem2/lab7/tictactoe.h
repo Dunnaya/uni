fd0063699e2463582fef1ec3e3f2ad1f4b05ea2b
Dana <159054921+Dunnaya@users.noreply.github.com>
2024-05-17 21:00:56 +0300

    Update tictactoe.h

--- sem2/lab7/tictactoe.h
+++ sem2/lab7/tictactoe.h
@@ -10,7 +10,7 @@ void resetBoard();
 void printBoard();
 int checkFreeSpaces();
 void playerMove(bool);
-void computerMove();
+void computerMove(bool showLogic);
 char checkWinner();
 void printWinner(char, bool);
 void clearConsole();
@@ -20,7 +20,7 @@ char getValidInputForPlayAgain();
 
 int minimax(bool maximizing, int alpha, int beta, int depth, bool);
 
-void startGame(bool);
+void startGame(bool, bool);
 void playAgain();
 
 void menu();
