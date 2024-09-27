fd2840c849a666a76248442bd5636aa213300741
Dana <holydunnaya@gmail.com>
2024-09-27 16:08:37 +0300

    made a lil plan of the project

--- sem3/test1.cpp
+++ sem3/test1.cpp
@@ -1,5 +1,54 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <algorithm>
+#include <fstream>
+
+class Note
+{
+    protected:
+    std::string content;
+    std::time_t createdAt;
+    std::vector<std::string> tags;
+
+    public:
+    Note(const std::string& text) : content(text) 
+    {
+        createdAt = std::time(nullptr);
+    }
+
+    void addTag();
+    void removeTag();
+    bool hasTag();
+
+    void clear()
+    {
+        content.clear();
+    }
+
+    std::time_t getCreatedAt() const 
+    {
+        return createdAt;
+    }
+
+    std::string getContent() const 
+    {
+        return content;
+    }
+};
+
+class Notebook;
+class MarkdownNote;
+class PlainTextNote;
+class Tags;
+class File;
+class MarkdownFile;
+class PlainTextFile;
+class FileManager;
+class PDFExporter;
+
 int main()
 {
-    std::cout<<"bye";
+    std::cout<<"puke";
 }
