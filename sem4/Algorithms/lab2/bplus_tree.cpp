74f20d24c41eb11c6878f5e21d36b5833f5c8eb3
Dana <holydunnaya@gmail.com>
2025-03-06 00:32:29 +0200

    web sem 4 ex

--- sem4/Algorithms/lab2/bplus_tree.cpp
+++ sem4/Algorithms/lab2/bplus_tree.cpp
@@ -1,5 +1,37 @@
 /*Реалізувати В+ - дерево*/
 #include <iostream>
+#include <ostream>
+#include <vector>
+using namespace std;
+
+struct Complex 
+{
+    double real;
+    double imaginary;
+
+    Complex(double r, double i) : real(r), imaginary(i) {}
+
+    Complex operator+(const Complex& other) const 
+    {
+        return Complex(real + other.real, imaginary + other.imaginary);
+    }
+
+    bool operator<(const Complex& other) const 
+    {
+        return sqrt(real * real + imaginary * imaginary) < sqrt(other.real * other.real + other.imaginary * other.imaginary);
+    }
+
+    bool operator==(const Complex& other) const 
+    {
+        return real == other.real && imaginary == other.imaginary;
+    }
+
+    friend ostream& operator<<(ostream& os, const Complex& c) 
+    {
+        os << "(" << c.real << ", " << c.imaginary << ")";
+        return os;
+    }
+};
 
 int main() {
     return 0;
--- sem4/Algorithms/lab2/bst.cpp
+++ sem4/Algorithms/lab2/bst.cpp
@@ -1,5 +1,47 @@
 /*Реалізувати оптимальне бінарне дерево пошуку (динамічне програмування)*/
 #include <iostream>
+#include <ostream>
+#include <vector>
+using namespace std;
+
+struct Complex 
+{
+    double real;
+    double imaginary;
+
+    Complex(double r, double i) : real(r), imaginary(i) {}
+
+    Complex operator+(const Complex& other) const 
+    {
+        return Complex(real + other.real, imaginary + other.imaginary);
+    }
+
+    bool operator<(const Complex& other) const 
+    {
+        return sqrt(real * real + imaginary * imaginary) < sqrt(other.real * other.real + other.imaginary * other.imaginary);
+    }
+
+    bool operator==(const Complex& other) const 
+    {
+        return real == other.real && imaginary == other.imaginary;
+    }
+
+    friend ostream& operator<<(ostream& os, const Complex& c) 
+    {
+        os << "(" << c.real << ", " << c.imaginary << ")";
+        return os;
+    }
+};
+
+struct TreeNode
+{
+    Complex key;
+    double weight;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(const Complex& k, double w) : key(k), weight(w), left(nullptr), right(nullptr) {}
+};
 
 int main() {
     return 0;
