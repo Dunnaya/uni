e043174068d4a7f8b9834dad7ea86aa47b5f8f9b
Dana <holydunnaya@gmail.com>
2025-03-06 21:04:55 +0200

    added and set the old project

--- sem4/OOOP/lab1/lab1_a/old_project/backend/models/product.model.js
+++ sem4/OOOP/lab1/lab1_a/old_project/backend/models/product.model.js
@@ -1,28 +1,25 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  price: 
-  {
-    type: Number,
-    required: true
-  },
-  image: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: false
-  }
-},
-{
-    timestamps: true // createdAt, updatedAt
-});
+const productSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		image: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true, // createdAt, updatedAt
+	}
+);
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 export default Product;
--- sem4/OOOP/lab1/lab1_a/old_project/backend/server.js
+++ sem4/OOOP/lab1/lab1_a/old_project/backend/server.js
@@ -1,86 +1,30 @@
-//const express = require('express')
-// "type": "module", in .json file to use import/export syntax
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
-import mongoose, { mongo } from 'mongoose';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// app.get("/products",(req, res) => {
-//     res.send("Server is ready");
-// });
+const __dirname = path.resolve();
 
-app.use(express.json()); //allows to accept JSON data in the req.body
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.get("/api/products", async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({success: true, data: products});
-    } catch (error) {
-        console.log("Error in fetching products", error.message);
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
-app.post("/api/products", async (req, res) => {
-    const product = req.body; // user will send the data
+app.use("/api/products", productRoutes);
 
-    const newProduct = new Product(product);
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: "Please provide all fields" });
-    }
-
-    try {
-        await newProduct.save();
-        res.status(201).json({success: true, data: newProduct});
-    }
-    catch (error) {
-        console.error("Error in creating product", error.message);
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
-
-app.put("/api/products/:id", async (req, res) => {
-    const {id} = req.params;
-
-    const product = req.body;
-
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({success: false, message: "Invalid Product ID"});
-    }
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true});
-        res.status(200).json({success: true, data: updatedProduct});
-    } catch (error) {
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-    const {id} = req.params;
-    // console.log("id:", id);
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success: true, message: "Product deleted"});
-    } catch (error) {
-        console.log("Error in deleting product", error.message);
-        res.status(404).json({success: false, message: "Product not found"});
-    }
-});
-
-//Postman app for testing APIs
-//http://localhost:3000/api/products
-//POST request
-//Body -> raw -> JSON
-
-//console.log(process.env.MONGO_URI);
-
-app.listen(3000, () => {
-    connectDB();
-    console.log("Server started at http://localhost:3000");
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
 });
