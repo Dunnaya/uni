2fd379e1de554d3fe4261689b1321e149081429e
Dana <holydunnaya@gmail.com>
2025-03-11 23:01:36 +0200

    have a problem with using import statement outside a module

--- sem4/OOOP/lab1/lab1_a/new_project/backend/__tests__/product.controller.test.js
+++ sem4/OOOP/lab1/lab1_a/new_project/backend/__tests__/product.controller.test.js
@@ -1,6 +1,6 @@
 //== When writing the program Postman was used for testing instead of unit tests. ===
 
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import request from 'supertest';
 import express from 'express';
@@ -10,11 +10,11 @@ import productRoutes from '../routes/product.route.js';
 let mongoServer;
 const app = express();
 
-//setting express app for tests
+//setting Express app for tests
 app.use(express.json());
 app.use('/api/products', productRoutes);
 
-//settings before all tests
+//DB settings before all tests
 beforeAll(async () => {
     //creating in-memory MongoDB server
     mongoServer = await MongoMemoryServer.create();
@@ -24,14 +24,57 @@ beforeAll(async () => {
     await mongoose.connect(uri);
 });
 
-afterEach();
+//cleaning after each test
+afterEach(async () => {
+    await Product.deleteMany({});
+});
 
-afterAll();
+//disconnect and close after all tests
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+});
 
 //tests for GETting products
+describe('GET /api/products', () => {
+    
+});
 
 //tests for POST
+describe('POST /api/products', () => {
+    it('must return an empty arr if no products', async () => {
+        const res = await request(app).get('/api/products');
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body.success).toBe(true);
+        expect(res.body.data).toBeInstanceOf(Array);
+        expect(res.body.data.length).toBe(0);
+    });
+
+    it('must return the products array', async () => {
+        //creating test product
+        await Product.create({
+            name: 'Test product',
+            price: 999,
+            image: 'test-image.png'
+        });
+
+        const res = await request(app).get('/api/products');
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body.success).toBe(true);
+        expect(res.body.data).toBeInstanceOf(Array);
+        expect(res.body.data.length).toBe(1);
+        expect(res.body.data[0].name).toBe('Test product');
+    });
+});
 
 //tests for PUT
+describe('PUT /api/products', () => {
+    
+});
 
 //tests for DELETE
+describe('DELETE /api/products', () => {
+    
+});
