47b58d6befb5d2c6f7b4ddbb121e87c248eb37c7
Dana <holydunnaya@gmail.com>
2025-03-14 18:24:22 +0200

    added some uml diagrams

--- sem4/OOOP/lab1/lab1_a/new_project/backend/routes/product.route.js
+++ sem4/OOOP/lab1/lab1_a/new_project/backend/routes/product.route.js
@@ -10,7 +10,6 @@ import { protect, authorize } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.get("/", getProducts);
-
 router.post("/", protect, authorize("admin"), createProduct);
 router.put("/:id", protect, authorize("admin"), updateProduct);
 router.delete("/:id", protect, authorize("admin"), deleteProduct);
