95e8f45a11dc07e51a29f0ae7efc4b0000967971
Dana <holydunnaya@gmail.com>
2026-05-19 00:58:12 +0300

    oop lab1 fix

--- sem6/OOP/lab1/lab1/target/generated-sources/annotations/com/lab1/mapper/ProductMapperImpl.java
+++ sem6/OOP/lab1/lab1/target/generated-sources/annotations/com/lab1/mapper/ProductMapperImpl.java
@@ -6,8 +6,8 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-05-17T21:44:50+0300",
-    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.46.0.v20260407-0427, environment: Java 21.0.10 (Eclipse Adoptium)"
+    date = "2026-05-19T00:55:54+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 24.0.2 (Homebrew)"
 )
 public class ProductMapperImpl implements ProductMapper {
 
